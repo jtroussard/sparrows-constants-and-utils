6bb23607aa11e2705a834babd55aa902b8cb9fd4
jacques troussard <jacques.troussard@mckesson.com>
2021-05-26 20:02:07 -0400

    trying to package

---
+++ setup.py
@@ -0,0 +1,12 @@
+setup(
+    name='sparrows-constants-and-utilities',
+    version='0.0.0',
+    url='https://github.com/jtroussard/sparrows-constants-and-utils.git',
+    classifiers = [...],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[],
+    packages=['sprw-const-n-utils'],
+    py_modules=[''],
+    scripts=['']
+)
